fc3eadb0353a3640bc46cf96d74de88b86e08a6b
Dan Maharry <danm@hmobius.com>
2018-02-25 16:56:37 +0000

    Initial copy of Converter code to update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
 using Newtonsoft.Json;
 
 namespace BlogML2Ghost.Core
@@ -14,7 +17,7 @@ namespace BlogML2Ghost.Core
             // TODO Nicer command line here - pull proper command line parser?
             if (args.Length != 2)
             {
-                Console.WriteLine("Invalid arguments");
+                Console.WriteLine("Usage: blogml2ghost <input.xml> <output.json>");
                 return;
             }
 
@@ -50,8 +53,73 @@ namespace BlogML2Ghost.Core
 
         private static object ConvertInputFile(string input)
         {
-            throw new NotImplementedException();
+            XNamespace xNamespace = "http://www.blogml.com/2006/09/BlogML";            
+
+            var root = XElement.Load(File.OpenRead(input));            
+            var postRoot = root.Element(xNamespace + "posts");
+            var posts = postRoot.Elements(xNamespace + "post");
+            var convertedPosts = new List<object>();
+            var tagsBelongingToPost = new List<object>();
+
+            var categories = root.Element(xNamespace + "categories")
+                .Elements(xNamespace + "category")
+                .Select((p, s) => new {p, s })
+                .ToDictionary(t => (Guid)t.p.Attribute("id"), v => new
+                {
+                    id = v.s,
+                    name = v.p.Element(xNamespace + "title").Value,
+                    slug = GetSlugFromTitle(v.p.Element(xNamespace + "title").Value),
+                    description = ""
+                });
+
+            int postId = 0;            
+            foreach (var post in posts)
+            {
+                postId++;
+                convertedPosts.Add(new
+                {
+                    id = postId,
+                    uuid = Guid.NewGuid(),
+                    title = post.Element(xNamespace + "title").Value,
+                    slug = GetSlugFromTitle(post.Element(xNamespace + "title").Value),
+                    markdown = post.Element(xNamespace + "content").Value,
+                    html = post.Element(xNamespace + "content").Value,
+                    image = (object)null,
+                    featured = 0,
+                    page = 0,
+                    status = "published",
+                    language = "en_GB",
+                    meta_title = (object)null,
+                    meta_description = (object)null,
+                    author_id = 1,
+                    created_at = GetEpochTime((DateTime)post.Attribute("date-created")),
+                    created_by = 1,
+                    updated_at = GetEpochTime((DateTime)post.Attribute("date-modified")),
+                    updated_by = 1,
+                    published_at = GetEpochTime((DateTime)post.Attribute("date-created")),
+                    published_by = 1
+                });
+
+                var categoryTags = post.Element(xNamespace + "categories");
+                if (categoryTags != null)
+                {
+                    var id = postId;
+                    var tags = categoryTags
+                        .Elements(xNamespace + "category")
+                        .Select(x => new { post_id = id, tag_id = categories[(Guid)x.Attribute("ref")].id });
+
+                    tagsBelongingToPost.AddRange(tags);
+                }
+            }
+
+            return new
+            {
+                posts = convertedPosts,
+                tags = categories.Values.ToList(),
+                posts_tags = tagsBelongingToPost
+            };
         }
+
 
         // TODO Rewrite as DateTime extenstion AsMillisecondsSinceEpoch
         private static long GetEpochTime(DateTime dateTime)
