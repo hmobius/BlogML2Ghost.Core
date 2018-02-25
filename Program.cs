f1a5ea35f8851b89657167092de3ff9dff7abbcc
Dan Maharry <danm@hmobius.com>
2018-02-25 16:38:45 +0000

    First upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace BlogML2Ghost.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO Nicer command line here - pull proper command line parser?
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalid arguments");
+                return;
+            }
+
+            string inputFileName= args[0];
+            string outputFileName = args[1];
+
+            // TODO Pull out ghost importformat into external classes
+            const string DataVersion = "003";
+
+            var metaData = new {
+                exported_on = GetEpochTime(DateTime.UtcNow),
+                version = DataVersion
+            };
+
+            // TODO Make conversion process more explicit
+            var convertedData = ConvertInputFile(inputFileName);
+
+            // TODO Split serailization into function
+            var rootData = new {
+                meta = metaData,
+                data = convertedData
+            };
+
+            string jsonData = JsonConvert.SerializeObject(rootData, 
+                new JsonSerializerSettings{
+                    NullValueHandling = NullValueHandling.Include
+                });
+
+            File.WriteAllText(outputFileName, jsonData);
+
+            Console.WriteLine("Success!");
+        }
+
+        private static object ConvertInputFile(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO Rewrite as DateTime extenstion AsMillisecondsSinceEpoch
+        private static long GetEpochTime(DateTime dateTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return (long)(dateTime - epoch).TotalMilliseconds;
+        }
+
+        // TODO Rewrite within BlogML class?
+        private static string GetSlugFromTitle(string url)
+        {
+            return Regex.Replace(Regex.Replace(RemoveDiacritics(url), "[^A-Za-z0-9-]+", "-"), "-{2,}", "-");
+        }
+        
+        public static string RemoveDiacritics(string text)
+        {
+            Encoding srcEncoding = Encoding.UTF8;
+            Encoding destEncoding = Encoding.GetEncoding(1252); // Latin alphabet
+
+            text = destEncoding.GetString(Encoding.Convert(srcEncoding, destEncoding, srcEncoding.GetBytes(text)));
+
+            string normalizedString = text.Normalize(NormalizationForm.FormD);
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < normalizedString.Length; i++)
+            {
+                if (!CharUnicodeInfo.GetUnicodeCategory(normalizedString[i]).Equals(UnicodeCategory.NonSpacingMark))
+                {
+                    builder.Append(normalizedString[i]);
+                }
+            }
+
+            return builder.ToString();
+        }        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # BlogML2Ghost.Core
 .NET Core Converter for BlogML files to Ghost-compatible JSON
+
+BlogML was created as an export format to port blog content between blog engines andstorage providers.
+
+ is supported by many .NET blogging tools such as 
+
+- [Telligent Community](http://telligent.com)
+- [BlogEngine.NET](https://francis.bio/blogengine/)
+- [DasBlog](https://github.com/shanselman/dasblog)
+- [The Orchard Project](http://orchardproject.net/) 
+
+Also there are some implementations for other blogging engines and services such as Blogger and Wordpress. More information can be found at [BlogML's home on Google Code](https://code.google.com/archive/p/blogml/)
+
+
+----
+
+Ghost is an open source blog platform at ghost.org. It uses its own custom JSON import \ export format [detailed here](https://docs.ghost.org/docs/the-importer#section-rolling-your-own).
